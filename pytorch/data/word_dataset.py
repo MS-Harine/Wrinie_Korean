2c0fd516c05d1f5b86c7ee98fd3d6a751515c1c1
MS-Harine <Minseok.H.Song@gmail.com>
2021-06-06 23:32:55 +0900

    Test encoder-decoder

--- pytorch/data/word_dataset.py
+++ pytorch/data/word_dataset.py
@@ -1,11 +1,12 @@
 from torch.utils.data import Dataset
 from .font2img import draw_single_char
 import torch
+import re
 
 class WordImageDataset(Dataset):
 	def __init__(self, string, source_font_path, target_fonts_path,
 				 size=128, transform=None):
-		self.string = string
+		self.string = re.sub('\s+', '', string)
 		self.source_font = source_font_path
 		self.target_fonts = target_fonts_path
 		self.size = size
@@ -20,14 +21,14 @@ class WordImageDataset(Dataset):
 
 		font_idx = idx // len(self.string)
 		ch_idx = idx % len(self.string)
-		
+
 		ch = self.string[ch_idx]
 		source_img = draw_single_char(ch, self.source_font, self.size)
 		target_img = draw_single_char(ch, self.target_fonts[font_idx], self.size)
 
 		if self.transform:
 			target_img = self.transform(target_img)
-		
+
 		return source_img, target_img, font_idx
 	
 	def get_categories(self):
